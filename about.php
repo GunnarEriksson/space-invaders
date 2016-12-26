a4c3aa4832e2948960d90e476b4f2568fd26faf4
Gunnar Eriksson <gunnar.t.eriksson@gmail.com>
2016-12-26 18:55:47 +0100

    Updated the presentation of the website

--- about.php
+++ about.php
@@ -5,9 +5,23 @@ include(__DIR__ . "/incl/header.php");
 ?>
 <article>
     <h1>Om spelsidan</h1>
-    <p>En kort presentation av spelsidan med en liten information om mig själv
-    som finns på alla andra projektarbeten.</p>
+    <p>Det här är sidan för dig som gillar att spela det klassiska arkadspelet
+    Space Invaders, som går ut på att hindra utomjordingarna att ta över jorden.</p>
+    <p>Spelet är ett avslutande projekt i kursen JavaScript, jQuery och AJAX med
+    HTML5 och PHP (DV1483) som är den avslutande kursen i kurspaketet
+    “Databaser, HTML, CSS, JavaScript och PHP” (30 hp) som ges av Blekinge
+    Tekniska Högskola (BTH).</p>
 </article>
+<footer class="byline">
+    <p>
+        <img src="./img/me/byline.jpg" alt="En bild på Gunnar Eriksson">
+        Webbsidan är skapad av Gunnar Eriksson i ett kursmoment som avslutar
+        och eximinerar kursen JavaScript, jQuery och AJAX med HTML5 och PHP
+        som ges av Blekinge Tekniska Högskola (BTH). Gunnar har arbetat med
+        systemutveckling i språken C++ och Java under många år. Studerar nu
+        webbprogrammering vid BTH.
+    </p>
+</footer>
 
 <?php $path=__DIR__; include(__DIR__ . "/incl/footer.php"); ?>
 <?php $path=__DIR__; include(__DIR__ . "/incl/end-of-page.php"); ?>
--- index.php
+++ index.php
@@ -5,12 +5,19 @@ include(__DIR__ . "/incl/header.php");
 ?>
 <article>
     <h1>Välkommen till spelsidan Space Invaders</h1>
+    <img src="./img/index/si_intro.jpg" alt="Bilder från spelet som visar intro, spelet och poänglistan">
     <p>Här är sidan för alla som gillar att spela det klassiska arkadspelet
     Space Invaders.</p>
     <p>Det klassiska spelet som går ut på att med en kanon försöka hindra
-    utomjordingarna från att ta över jorden.</p>
-    <p>Om du registrerar dig på spelsidan, kan du vara med om att tävla om den
-    översta platsen på poänglistan.</p>
+    utomjordingarna från att ta över jorden. Om utomjordingarna lyckas förstöra
+    din kanon, så har du ytterligare två kanoner som du kan använda. Dock bara
+    en åt gången. Du får olika poäng för varje utomjording du skjuter ner beroende
+    på vilket typ av utomjording det är. Lyckas du skjuta ner det mystiska skeppet
+    som dyker upp då och då, så får du extra poäng. När utomjordingarna har förstört
+    alla dina kanoner eller har nått jorden är spelet över. Då kan du skriva in
+    ditt namn och kanske hamna överst i poänglistan.</p>
+    <p>Hur du spelar spelet, visas under introduktionsrutan i menyn spel.</p>
+    <p>Lycka till!</p>
 </article>
 
 <?php $path=__DIR__; include(__DIR__ . "/incl/footer.php"); ?>
